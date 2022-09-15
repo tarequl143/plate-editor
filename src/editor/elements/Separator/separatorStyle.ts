6ed72555738fba0627005e389aad94fe79399382
aakash1122 <aakaashhossain@gmail.com>
2022-09-15 14:59:51 +0600

    css: update separator color

--- src/editor/elements/Separator/separatorStyle.ts
+++ src/editor/elements/Separator/separatorStyle.ts
@@ -4,7 +4,7 @@ export const SeparatorWrapper = styled.div<{ focused: boolean }>`
   padding: 12px 10px;
   background-color: ${(props) => (props.focused ? "#f1f1f1" : "transparent")};
   & hr {
-    border-top: 1px solid ${(props) => (props.focused ? "red" : "#f1f1f1")};
+    border-top: 1px solid ${(props) => (props.focused ? "red" : "#E5E7EB")};
     background-color: transparent;
     height: 0px;
   }
