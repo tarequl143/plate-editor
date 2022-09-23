4724dc34b8a8abf5c5817d71475c12d74b94d7a8
aakash1122 <aakaashhossain@gmail.com>
2022-09-23 16:22:58 +0600

    replace checkbox with tixio ui checkbox

--- src/editor/elements/Todolist/TodoListElement.tsx
+++ src/editor/elements/Todolist/TodoListElement.tsx
@@ -1,6 +1,7 @@
+import { Checkbox } from "@getonnet/tixio-ui-core";
 import { findNodePath, setNodes, usePlateEditorRef } from "@udecode/plate";
 import { useFocused, useReadOnly, useSelected } from "slate-react";
-import { Checkbox, CheckboxWrapper, ElementWrapper } from "./TodoListStyle";
+import { ElementWrapper } from "./TodoListStyle";
 
 const TodoListElement = (props: any) => {
   const editor = usePlateEditorRef()!;
@@ -11,30 +12,22 @@ const TodoListElement = (props: any) => {
 
   const { checked } = element;
 
+  const toggleCheckbox = (event: any) => {
+    if (!editor || readOnly) return;
+
+    const path = findNodePath(editor, element);
+    const newProperties: any = {
+      checked: event.target.checked,
+    };
+    setNodes(editor, newProperties, { at: path });
+  };
+
   return (
     <ElementWrapper
       {...attributes}
       className={`checklist-element ${selected && focused ? "selected" : ""}`}
     >
-      <CheckboxWrapper contentEditable={false} suppressContentEditableWarning>
-        <input
-          type="checkbox"
-          checked={checked}
-          onChange={(event) => {
-            if (!editor || readOnly) return;
-
-            const path = findNodePath(editor, element);
-            const newProperties: any = {
-              checked: event.target.checked,
-            };
-            setNodes(editor, newProperties, { at: path });
-          }}
-        />
-        <Checkbox />
-      </CheckboxWrapper>
-      <span contentEditable={!readOnly} suppressContentEditableWarning>
-        {children}
-      </span>
+      <Checkbox checked={checked} onChange={toggleCheckbox} label={children} />
     </ElementWrapper>
   );
 };
